538707a341100917c2cb8776f3f11640283baa3d
john <john@hesse>
2012-02-17 23:17:30 +0100

    maybe unfinished

--- www/js/pybliothek.js
+++ www/js/pybliothek.js
@@ -1,92 +1,97 @@
 (function($) {
-    
-    var Book = Backbone.Model.extend ({
+
+    var Book = Backbone.Model.extend({
         defaults : {
             db_id : null,
-            isbn: null,
+            isbn : null,
             author : null,
-            title: null,
+            title : null,
             inserted : null
         }
     });
-    
-    var BookList = Backbone.Collection.extend ({
-        model: Book
+
+    var BookList = Backbone.Collection.extend({
+        model : Book
     });
-    
-    var BookView = Backbone.View.extend ({
-        tagName: 'tr',
-        template : $('#template .tmpl_book table tr').template (),
-        render : function () {
-            console.log ('book render');
-            console.log (this.model.toJSON());
-            $(this.el).html ($.tmpl (this.template, this.model.toJSON ()));
+
+    var BookView = Backbone.View.extend({
+        tagName : 'tr',
+        template : $('#template .tmpl_book table tr').template(),
+        render : function() {
+            
+            $(this.el).html($.tmpl(this.template, this.model.toJSON()));
             return this;
         }
     });
+
     
-    
-    var PybliothekView = Backbone.View.extend ({
-        el: $('#tbl_placeholder'),
-        template : $('#template .tmpl_book_list').template(),
-        initialize : function () {
-            
-            _.bindAll(this, 'load', 'updateContent', 'render');
-            
-            this.collection = new BookList ();
-            this.collection.bind ('reset', this.render);
-            
-        },
+    var BookAddView = Backbone.View.extend ({
         
+    });
+    
+    var PybliothekView = Backbone.View.extend({
+        el : $('#tbl_placeholder'),
+        template : $('#template .tmpl_book_list').template(),
+        initialize : function() {
+
+            _.bindAll(this, 'load', 'updateContent', 'render');
+
+            this.collection = new BookList();
+            this.collection.bind('reset', this.render);
+
+        },
+
         events : {
             'click button#refresh' : 'load',
             'click button#add' : 'addBook'
         },
-        
-        addBook : function () {
-          alert ('add');  
+
+        addBook : function() {
+            var v = $('#template .book_add').template ();
+            console.log ($('#float_placeholder'));
+            $('#float_placeholder').append ($.tmpl(v));
         },
-        buildBookFromJSON : function (d) {
-            var b = new Book ();
-            
-            b.set ({
+        
+        buildBookFromJSON : function(d) {
+            var b = new Book();
+
+            b.set({
                 isbn : d.isbn,
                 author : d.author,
-                title: d.title,
+                title : d.title,
                 id : d.id
             });
             return b;
         },
-        updateContent : function (data) {
-            console.log (data);
-            this.collection.reset (_.map (JSON.parse (data), this.buildBookFromJSON ));
+        updateContent : function(data) {
+            console.log(data);
+            this.collection.reset(_.map(JSON.parse(data),
+                    this.buildBookFromJSON));
         },
-        
-        render : function () {
-           
-            $(this.el).html ($.tmpl (this.template));
-            _(this.collection.models).each (function (i) {
-                var bv = new BookView ({
-                    model: i
+
+        render : function() {
+
+            $(this.el).html($.tmpl(this.template));
+            _(this.collection.models).each(function(i) {
+                var bv = new BookView({
+                    model : i
                 });
-                $ ('.list', this.el).append (bv.render().el);
+                $('.list', this.el).append(bv.render().el);
             });
-            
-            console.log ('render');
+
+            console.log('render');
         },
-        
-        load: function () {
-            $.ajax ({
-                type: 'GET',
-                url: '/book',
-                success: this.updateContent
+
+        load : function() {
+            $.ajax({
+                type : 'GET',
+                url : '/book',
+                success : this.updateContent
             });
         }
     });
-    
-    
-     var pybliothek = new PybliothekView ();
-     pybliothek.load();
+
+    var pybliothek = new PybliothekView();
+    pybliothek.load();
 
 })(jQuery);
-
